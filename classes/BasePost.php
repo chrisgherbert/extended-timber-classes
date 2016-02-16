d2b5a20bdf7c9213e34560ec0ecab655c0f46a52
Chris Herbert <herbert@bermanco.com>
2016-02-15 21:15:32 -0500

    Initial commit

---
+++ classes/BasePost.php
@@ -0,0 +1,439 @@
+<?php
+
+use bermanco\Posts2PostsAdapter\Posts2PostsAdapter;
+
+class BasePost extends TimberPost {
+
+	public $PostClass = 'BasePost';
+	public $disable_robots = false;
+	public $custom_field_prefix = '';
+
+	protected $p2p_adapter;
+
+	public function __construct(
+		$pid = null,
+		Posts2PostsAdapter $p2p_adapter = null
+	) {
+
+		parent::__construct($pid);
+
+		if ($p2p_adapter){
+			$this->p2p_adapter = $p2p_adapter;
+		}
+		else {
+			$this->p2p_adapter = Posts2PostsAdapter::create();
+		}
+
+	}
+
+	/**
+	 * Gets the label of a post type, if it is defined.
+	 * @param  string $label Post type slug
+	 * @return string        The desired post type label
+	 */
+	public function get_post_type_label($label){
+
+		$post_type_obj = get_post_type_object($this->post_type);
+
+		if ($post_type_obj){
+
+			if (isset($post_type_obj->labels->{$label})){
+				return $post_type_obj->labels->{$label};
+			}
+
+		}
+
+	}
+
+	/**
+	 * Check if the image is large enough to be expanded to fill a content
+	 * container (at least 500px wide)
+	 * @return boolean True if the image is at least 500px wide
+	 */
+	public function has_large_thumbnail(){
+
+		$thumbnail = $this->get_thumbnail();
+
+		if ($thumbnail && $thumbnail->width() > 499){
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	}
+
+	/**
+	 * Get the first image embedded in the post's content
+	 * @return string|null URL of the first content image
+	 */
+	public function get_first_content_image(){
+
+		$first_img = '';
+
+		$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/U', $this->get_content(), $matches);
+
+		if (isset($matches[1][0])){
+			$first_img = $matches[1][0];
+		}
+
+		if ($first_img){
+			return $first_img;
+		}
+
+	}
+
+	/**
+	 * Get a string with comma-separated list of the post's tag names
+	 * @return string Tag names list
+	 */
+	public function get_tags_links_string(){
+
+		$tags = $this->get_terms('tag');
+
+		if ($tags){
+			return $this->create_posts_links_string($tags);
+		}
+
+	}
+
+	/**
+	 * Get a string with comma-separated list of the post's category names
+	 * @return string Category names list
+	 */
+	public function get_categories_links_string(){
+
+		$categories = $this->get_terms('category');
+
+		if ($categories){
+			return $this->create_posts_links_string($categories);
+		}
+
+	}
+
+	/**
+	 * Get the PHP class of the current object
+	 * @return string PHP class name
+	 */
+	public function get_php_class(){
+		return get_class($this);
+	}
+
+	/**
+	 * Get Facebook Share URL
+	 * @return string Facebook Share URL
+	 */
+	public function get_facebook_share_url(){
+		$base = 'https://www.facebook.com/sharer/sharer.php?u=';
+		$url = rawurlencode($this->get_link());
+		return $base . $url;
+	}
+
+	/**
+	 * Get Twitter Tweet URL
+	 * @return string Twitter Tweet URL
+	 */
+	public function get_twitter_tweet_url($handle = ''){
+
+		$format = 'https://twitter.com/intent/tweet?text=%s&url=%s&via=%s';
+
+		$url_params = array();
+
+		$url_params['text'] = $this->get_title();
+		$url_params['url'] = $this->get_link();
+
+		if ($handle){
+			$url_params['via'] = $handle;
+		}
+
+		$query_string = http_build_query($url_params, false, '&', PHP_QUERY_RFC3986);
+
+		return "https://twitter.com/intent/tweet?$query_string";
+
+	}
+
+	/**
+	 * Get mailto link
+	 * @return string The mailto link
+	 */
+	public function get_mailto_url(){
+		$format = 'mailto:?subject=%s&body=%s';
+		$subject = rawurlencode($this->get_title());
+		$body = rawurlencode('Check it out: ' . $this->get_link());
+		return sprintf($format, $subject, $body);
+	}
+
+	/////////
+	// RSS //
+	/////////
+
+	/**
+	 * Get post title, formatted for an RSS feed
+	 * @return string|null Formatted post title
+	 */
+	public function get_rss_title(){
+		$title = $this->get_title();
+		if ($title){
+			return html_entity_decode( $title, ENT_COMPAT, 'UTF-8' );
+		}
+	}
+
+	/**
+	 * Get content formatted and sanitized for an RSS feed.  RSS only allows 
+	 * specific tags, though I haven't been able to find a complete list of 
+	 * them.
+	 * @return string|null Post content prepared for RSS
+	 */
+	public function get_rss_content(){
+
+		$content = $this->get_content();
+
+		if ($content){
+
+			$allowed_tags = array(
+				'<p>',
+				'<a>',
+				'<strong>',
+				'<em>',
+				'<i>',
+				'<b>',
+				'<br>',
+				'<div>',
+				'<ul>',
+				'<li>',
+				'<blockquote>',
+				'<table>',
+				'<h1>',
+				'<h2>',
+				'<address>',
+				'<article>',
+				'<header>',
+				'<img>',
+				'<legend>',
+				'<ol>',
+				'<pre>',
+				'<code>',
+				'<small>',
+				'<span>',
+				'<table>',
+				'<tr>',
+				'<td>',
+				'<th>',
+				'<thead>',
+				'<tbody>',
+				'<sub>',
+				'<u>'
+			);
+
+			$allowed_tags_string = implode('', $allowed_tags);
+
+			$stripped_content = strip_tags($content, $allowed_tags_string);
+
+			return $stripped_content;
+
+		}
+
+	}
+
+	///////////////
+	// Protected //
+	///////////////
+
+	/**
+	 * Create a comma-separated list of post links
+	 * @param  array  $posts Array of TimberPost objects
+	 * @return string        List of comma separated links
+	 */
+	protected function create_posts_links_string(array $posts = null){
+
+		if ($posts){
+
+			$links = array();
+
+			foreach ($posts as $post){
+				$url = $post->get_link();
+				$title = $post->get_title();
+				$links[] = "<a href='$url'>$title</a>";
+			}
+
+			return implode(', ', $links);
+
+		}
+
+	}
+
+	/**
+	 * Create a comma-separated list of post titles from an array of post
+	 * objects
+	 * @param  array|null $posts AlecPost objects
+	 * @return string            List of comma-separated post titles
+	 */
+	protected function create_posts_titles_string(array $posts = null){
+
+		if ($posts){
+
+			$items = array();
+
+			foreach ($posts as $post){
+				$items[] = $post->get_title();
+			}
+
+			return implode(', ', $items);
+
+		}
+
+	}
+
+	/**
+	 * Create a comma-separated list of term titles from an array of
+	 * TimberTerm objects
+	 * @param  array|null $terms Array of TimberTerm objects
+	 * @return string            List of term titles
+	 */
+	protected function create_terms_titles_string(array $terms = null){
+
+		if ($terms){
+
+			$items = array();
+
+			foreach ($terms as $term){
+				$items[] = $term->name;
+			}
+
+			return implode(', ', $items);
+
+		}
+
+	}
+
+	protected function create_terms_links_string(array $terms = null){
+
+		if ($terms){
+
+			$items = array();
+
+			foreach ($terms as $term){
+				$name = $term->name;
+				$url = $term->get_link();
+				$items[] = "<a href='$url'>$name</a>";
+			}
+
+			return implode(', ', $items);
+
+		}
+
+	}
+
+	//////////
+	// CMB2 //
+	//////////
+
+	/**
+	 * Get the meta key for a CMB2 metadata field
+	 * @param  string $id CMB2 field id (not including prefix)
+	 * @return string     Complete WordPress meta key
+	 */
+	protected function get_cmb2_meta_key($id){
+		$meta_key = $this->custom_field_prefix . $id;
+		return $meta_key;
+	}
+
+	/**
+	 * Get CMB2 meta data
+	 * @param  string $id Meta key
+	 * @return mixed      Meta value
+	 */
+	public function get_cmb2_meta($id){
+		$key = $this->get_cmb2_meta_key($id);
+		return $this->$key;
+	}
+
+	/**
+	 * Get the TimberImage file attachment class from a CMB2 file attachment
+	 * field
+	 * @param  string $key      CMB2 field ID.  Should not include the prefix.
+	 * @return TimberImage|null      TimberImage object for the file attachment
+	 */
+	protected function get_cmb2_image($id){
+
+		$meta_data = $this->get_cmb2_meta($id);
+
+		if ($meta_data){
+			$file_obj = new TimberImage($meta_data);
+			return $file_obj;
+		}
+
+	}
+
+	/**
+	 * Get an attachment URL from a CMB2 file attachment field
+	 * @param  string $key     CMB2 field ID.  Should not include the prefix.
+	 * @return string|null     Attachment URL
+	 */
+	protected function get_cmb2_file_attachment_url($id){
+
+		$meta_data = $this->get_cmb2_meta($id);
+
+		if ($meta_data){
+			return wp_get_attachment_url($meta_data);
+		}
+
+	}
+
+	/////////////
+	// Aliases //
+	/////////////
+
+	// @codeCoverageIgnoreStart
+
+	function post_type_label($label){
+		$this->get_post_type_label($label);
+	}
+
+	function large_thumbnail(){
+		$this->has_large_thumbnail();
+	}
+
+	function first_content_image(){
+		$this->get_first_content_image();
+	}
+
+	function tags_links_string(){
+		$this->get_tags_links_string();
+	}
+
+	function categories_links_string(){
+		$this->get_categories_links_string();
+	}
+
+	function php_class(){
+		$this->get_php_class();
+	}
+
+	function facebook_share_url(){
+		$this->get_facebook_share_url();
+	}
+
+	function twitter_tweet_url($handle = ''){
+		$this->get_twitter_tweet_url($handle = '');
+	}
+
+	function mailto_url(){
+		$this->get_mailto_url();
+	}
+
+	function rss_title(){
+		$this->get_rss_title();
+	}
+
+	function rss_content(){
+		$this->get_rss_content();
+	}
+
+	function cmb2_meta($id){
+		$this->get_cmb2_meta($id);
+	}
+
+	// @codeCoverageIgnoreEnd
+
+}
---
+++ tests/test-BasePost.php
@@ -0,0 +1,307 @@
+<?php
+
+class BasePostTest extends WP_UnitTestCase {
+
+	public function test_get_php_class(){
+
+		$post_id = $this->factory->post->create();
+		$post = new BasePost($post_id);
+
+		$this->assertEquals('BasePost', $post->get_php_class());
+
+	}
+
+	/**
+	 * @dataProvider get_rss_title_provider
+	 */
+	public function test_get_rss_title($title){
+
+		$post_id = $this->factory->post->create(array(
+			'post_title' => $title
+		));
+		$post = new BasePost($post_id);
+
+		$this->assertEquals($title, $post->get_rss_title());
+
+	}
+
+	public function get_rss_title_provider(){
+
+		return array(
+			array("We’d all be better off without apostropes"),
+			array("In the ‘60s, rock ‘n’ roll"),
+			array("“That’s a ‘magic’ sock.”"),
+			array('')
+		);
+
+	}
+
+	/**
+	 * @dataProvider get_facebook_share_url_provider
+	 */
+	public function test_get_facebook_share_url($expected_value, $url){
+
+		$post_id = $this->factory->post->create();
+		$object = new ShareTestClass($post_id);
+		$object->link_value = $url;
+
+		$this->assertEquals($expected_value, $object->get_facebook_share_url());
+
+	}
+
+	public function get_facebook_share_url_provider(){
+
+		return array(
+			array("https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.alec.org%2Fpublication%2F2016opioids%2F","https://www.alec.org/publication/2016opioids/"),
+			array("https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.alec.org%2Farticle%2Fthreat-of-the-zika-virus%2F", "https://www.alec.org/article/threat-of-the-zika-virus/")
+		);
+
+	}
+
+	/**
+	 * @dataProvider get_twitter_tweet_url_provider
+	 */
+	public function test_get_twitter_tweet_url($expected_value, $url, $title, $handle){
+
+		$post_id = $this->factory->post->create();
+		$object = new ShareTestClass($post_id);
+		$object->link_value = $url;
+		$object->title_value = $title;
+
+		$this->assertEquals($expected_value, $object->get_twitter_tweet_url($handle));
+
+	}
+
+	public function get_twitter_tweet_url_provider(){
+
+		return array(
+			array("https://twitter.com/intent/tweet?text=custom%20share%20text&url=https%3A%2F%2Fdev.twitter.com%2Fweb%2Ftweet-button&via=twitterdev", "https://dev.twitter.com/web/tweet-button", "custom share text", "twitterdev")
+		);
+
+	}
+
+	/**
+	 * @dataProvider get_mailto_url_provider
+	 */
+	public function test_get_mailto_url($expected_value, $url, $title){
+
+		$post_id = $this->factory->post->create();
+		$object = new ShareTestClass($post_id);
+		$object->link_value = $url;
+		$object->title_value = $title;
+
+		$this->assertEquals($expected_value, $object->get_mailto_url());
+
+	}
+
+	public function get_mailto_url_provider(){
+
+		return array(
+			array("mailto:?subject=West%20Virginia%20Becomes%20the%2026th%20Right-to-Work%20State&body=Check%20it%20out%3A%20https%3A%2F%2Fwww.alec.org%2Farticle%2Fwest-virginia-becomes-the-26th-right-to-work-state%2F", "https://www.alec.org/article/west-virginia-becomes-the-26th-right-to-work-state/", "West Virginia Becomes the 26th Right-to-Work State")
+		);
+
+	}
+
+	/**
+	 * @dataProvider get_first_content_image_provider
+	 */
+	public function test_get_first_content_image($expected_value, $post_content){
+
+		$post_id = $this->factory->post->create(array('post_content' => $post_content));
+		$post = new BasePost($post_id);
+
+		$this->assertEquals($expected_value, $post->get_first_content_image());
+
+	}
+
+	public function get_first_content_image_provider(){
+
+		return array(
+			array('http://test.com/image1.jpg', '<p><img src="http://test.com/image1.jpg" alt="" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>'),
+			array(null, ' Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.'),
+			array('http://test.com/image1.jpg', '<p><img src="http://test.com/image1.jpg" alt="" />Lorem ipsum <img src="http://test.com/image2.jpg" alt="" />dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, <img src="http://test.com/image3.jpg" alt="" />posuere a, pede.</p>')
+		);
+
+	}
+
+	/**
+	 * @dataProvider create_posts_titles_string_provider
+	 */
+	public function test_create_posts_titles_string($expected_value, $post_array){
+
+		$post_id = $this->factory->post->create();
+		$post = new BasePost($post_id);
+
+		$reflection_class = new ReflectionClass("BasePost");
+		$reflection_method = $reflection_class->getMethod("create_posts_titles_string");
+		$reflection_method->setAccessible(true);
+		$result = $reflection_method->invoke($post, $post_array);
+
+		$this->assertEquals($expected_value, $result);
+
+	}
+
+	public function create_posts_titles_string_provider(){
+
+		$post_one = new BasePost($this->factory->post->create(array('post_title' => 'Title One')));
+		$post_two = new BasePost($this->factory->post->create(array('post_title' => 'Title Two')));
+		$post_three = new BasePost($this->factory->post->create(array('post_title' => 'Title Three')));
+
+		return array(
+			array('Title One, Title Two, Title Three', array($post_one, $post_two, $post_three)),
+			array('Title One, Title Two', array($post_one, $post_two)),
+			array('Title One', array($post_one)),
+			array(null, array()),
+		);
+
+	}
+
+	/**
+	 * @dataProvider create_terms_titles_string_provider
+	 */
+	public function test_create_terms_titles_string($expected_value, $terms){
+
+		$post = new BasePost($this->factory->post->create());
+
+		$reflection_class = new ReflectionClass("BasePost");
+		$reflection_method = $reflection_class->getMethod("create_terms_titles_string");
+		$reflection_method->setAccessible(true);
+		$result = $reflection_method->invoke($post, $terms);
+
+		$this->assertEquals($expected_value, $result);
+
+	}
+
+	public function create_terms_titles_string_provider(){
+
+		$term_1 = new TimberTerm($this->factory->term->create(array('name' => 'Term 1')));
+		$term_2 = new TimberTerm($this->factory->term->create(array('name' => 'Term 2')));
+		$term_3 = new TimberTerm($this->factory->term->create(array('name' => 'Term 3')));
+
+		return array(
+			array('Term 1, Term 2, Term 3', array($term_1, $term_2, $term_3)),
+			array('Term 1, Term 2', array($term_1, $term_2)),
+			array('Term 1', array($term_1)),
+			array(null, array())
+		);
+
+	}
+
+	/**
+	 * @dataProvider create_posts_links_string_provider
+	 */
+	public function test_create_posts_links_string($expected_value, $posts_array){
+
+		$post = new BasePost($this->factory->post->create());
+
+		$reflection_class = new ReflectionClass("BasePost");
+		$reflection_method = $reflection_class->getMethod("create_posts_links_string");
+		$reflection_method->setAccessible(true);
+		$result = $reflection_method->invoke($post, $posts_array);
+
+		$this->assertEquals($expected_value, $result);
+
+	}
+
+	public function create_posts_links_string_provider(){
+
+		$post_1 = $this->get_BasePost_mock_for_posts_links_string_test('Post 1', '/post_1.php');
+		$post_2 = $this->get_BasePost_mock_for_posts_links_string_test('Post 2', '/post_2.php');
+		$post_3 = $this->get_BasePost_mock_for_posts_links_string_test('Post 3', '/post_3.php');
+
+		return array(
+			array("<a href='/post_1.php'>Post 1</a>, <a href='/post_2.php'>Post 2</a>, <a href='/post_3.php'>Post 3</a>", array($post_1, $post_2, $post_3)),
+			array("<a href='/post_1.php'>Post 1</a>, <a href='/post_2.php'>Post 2</a>", array($post_1, $post_2)),
+			array("<a href='/post_1.php'>Post 1</a>", array($post_1)),
+			array(null, array())
+		);
+
+	}
+
+	/**
+	 * @dataProvider create_terms_links_string_provider
+	 */
+	public function test_create_terms_links_string($expected_value, $terms_array){
+
+		$post = new BasePost($this->factory->post->create());
+
+		$reflection_class = new ReflectionClass("BasePost");
+		$reflection_method = $reflection_class->getMethod("create_terms_links_string");
+		$reflection_method->setAccessible(true);
+		$result = $reflection_method->invoke($post, $terms_array);
+
+		$this->assertEquals($expected_value, $result);
+
+	}
+
+	public function create_terms_links_string_provider(){
+
+		$term_1 = $this->get_TimberTerm_mock_for_terms_links_string_test('Term 1', '/term_1.php');
+		$term_2 = $this->get_TimberTerm_mock_for_terms_links_string_test('Term 2', '/term_2.php');
+		$term_3 = $this->get_TimberTerm_mock_for_terms_links_string_test('Term 3', '/term_3.php');
+
+		return array(
+			array("<a href='/term_1.php'>Term 1</a>, <a href='/term_2.php'>Term 2</a>, <a href='/term_3.php'>Term 3</a>", array($term_1, $term_2, $term_3)),
+			array("<a href='/term_1.php'>Term 1</a>, <a href='/term_2.php'>Term 2</a>", array($term_1, $term_2)),
+			array("<a href='/term_1.php'>Term 1</a>", array($term_1)),
+			array(null, array()),
+		);
+
+	}
+	///////////////
+	// Protected //
+	///////////////
+
+	protected function get_BasePost_mock_for_posts_links_string_test($title, $url){
+
+		$mock = $this->get_BasePost_mock();
+		$mock->method('get_title')->willReturn($title);
+		$mock->method('get_link')->willReturn($url);
+
+		return $mock;
+
+	}
+
+	protected function get_BasePost_mock(){
+
+		$stub = $this->getMockBuilder('BasePost')->disableOriginalConstructor()->getMock();
+
+		return $stub;
+
+	}
+
+	protected function get_TimberTerm_mock_for_terms_links_string_test($title, $url){
+
+		$mock = $this->get_TimberTerm_mock();
+		$mock->name = $title;
+		$mock->method('get_link')->willReturn($url);
+
+		return $mock;
+
+	}
+
+	protected function get_TimberTerm_mock(){
+
+		$stub = $this->getMockBuilder('TimberTerm')->disableOriginalConstructor()->getMock();
+
+		return $stub;
+
+	}
+
+}
+
+class ShareTestClass extends BasePost {
+
+	public $link_value;
+	public $title_value;
+
+	public function get_link(){
+		return $this->link_value;
+	}
+
+	public function get_title(){
+		return $this->title_value;
+	}
+
+}
+
